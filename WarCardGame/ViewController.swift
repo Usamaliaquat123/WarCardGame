d767a28b4c8da2d828902b709e05e889a698652c
Obrienser <obrienser@gmail.com>
2021-06-23 06:07:53 -0700

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Сергей Масленников on 23.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
